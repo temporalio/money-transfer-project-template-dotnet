a46a030dc114dfadd04bf3c9f261d2c65b9bbcca
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-02-27 09:30:38 -0600

    formatting, cleanup, indentation

--- MoneyTransferWorker/RunWorker.cs
+++ MoneyTransferWorker/RunWorker.cs
@@ -2,44 +2,38 @@ using Temporalio.Client;
 using Temporalio.Worker;
 using MoneyTransferProject;
 
-public class Program
+// Create a client to connect to localhost on "default" namespace
+var client = await TemporalClient.ConnectAsync(new("localhost:7233"));
+
+// Cancellation token to shutdown worker on ctrl+c
+using var tokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += (_, eventArgs) =>
 {
-    public static async Task Main(string[] args)
-    {
-        // Create a client to connect to localhost on "default" namespace
-        var client = await TemporalClient.ConnectAsync(new("localhost:7233"));
-        
-        // Cancellation token to shutdown worker on ctrl+c
-        using var tokenSource = new CancellationTokenSource();
-        Console.CancelKeyPress += (_, eventArgs) =>
-        {
-            tokenSource.Cancel();
-            eventArgs.Cancel = true;
-        };
+    tokenSource.Cancel();
+    eventArgs.Cancel = true;
+};
 
-        // Create an instance of the activities since we have instance activities.
-        // If we had all static activities, we could just reference those directly.
-        var activities = new BankingActivities();
+// Create an instance of the activities since we have instance activities.
+// If we had all static activities, we could just reference those directly.
+var activities = new BankingActivities();
 
-        // Create a worker with the activity and workflow registered
-        using var worker = new TemporalWorker(
-            client, // client
-            new TemporalWorkerOptions(taskQueue: "MONEY_TRANSFER_TASK_QUEUE")
-                .AddActivity(activities.WithdrawAsync) // Register activities 
-                .AddActivity(activities.DepositAsync)
-                .AddActivity(activities.RefundAsync)
-                .AddWorkflow<MoneyTransferWorkflow>() // Register workflow
-        );
-        
-        // Run the worker until it's cancelled
-        Console.WriteLine("Running worker...");
-        try
-        {
-            await worker.ExecuteAsync(tokenSource.Token);
-        }
-        catch (OperationCanceledException)
-        {
-            Console.WriteLine("Worker cancelled");
-        }
-    }
+// Create a worker with the activity and workflow registered
+using var worker = new TemporalWorker(
+    client, // client
+    new TemporalWorkerOptions(taskQueue: "MONEY_TRANSFER_TASK_QUEUE")
+        .AddActivity(activities.WithdrawAsync) // Register activities 
+        .AddActivity(activities.DepositAsync)
+        .AddActivity(activities.RefundAsync)
+        .AddWorkflow<MoneyTransferWorkflow>() // Register workflow
+);
+
+// Run the worker until it's cancelled
+Console.WriteLine("Running worker...");
+try
+{
+    await worker.ExecuteAsync(tokenSource.Token);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Worker cancelled");
 }
